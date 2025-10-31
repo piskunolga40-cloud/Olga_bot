0caa70aec05f4aeab30b3ebf75cf59bc879379eb
piskunolga40-cloud <piskun.olga40@gmail.com>
2025-10-31 23:12:54 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot
 from flask import Flask, request
 
-TOKEN = 8392489985:AAFzxKRngsUkbPHQWjHX8glygdjL8JYhtTg
+TOKEN = "8392489985:AAFzxKRngsUkbPHQWjHX8glygdjL8JYhtTg"
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
