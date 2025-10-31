5f20ad0a01cbef13209ffe1c8a571835ac9f9087
piskunolga40-cloud <piskun.olga40@gmail.com>
2025-10-31 21:59:51 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,27 @@
 import telebot
 from flask import Flask, request
 
-TOKEN = 8392489985:AAFzxKRngsUkbPHQWjHX8glygdjL8JYhtTg
+TOKEN = "8392489985:AAFzxKRngsUkbPHQW..."  # твой токен
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home():
     return "Бот работает!"
+
+@app.route(f'/{TOKEN}', methods=['POST'])
+def webhook():
+    json_str = request.get_data().decode('UTF-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return '', 200
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.reply_to(message, "Привет! Я твой бот 🤖")
+    bot.reply_to(message, "Привет! Я бот, развернутый на Render!")
 
 if __name__ == "__main__":
-    bot.polling()
+    import os
+    bot.remove_webhook()
+    bot.set_webhook(url=f"https://olga-bot-1.onrender.com/{TOKEN}")
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
